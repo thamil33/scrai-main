a0e44fc4eb02cc7d88a34e9e69e079b2ed6c785a
tyler.hamilton <tyler.hamilton@drhamilton.info>
2025-10-27 22:11:41 +0000

    build: Untrack .idx directory

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,55 +1,23 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  # Service definitions
+  services = {
+    # Enable Docker for container management
+    docker.enable = true;
 
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
-  ];
-
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-    ];
-
-    # Enable previews
-    previews = {
+    # Configure PostgreSQL service
+    postgres = {
       enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
+      package = pkgs.postgresql_16; # Specify PostgreSQL version 16
+      extensions = [ "pgvector" ];    # Enable the pgvector extension
     };
 
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
+    # Enable Redis service
+    redis.enable = true;
   };
+
+  # Additional environment setup can go here
+  # For example, installing packages available in the terminal
+  packages = [
+    pkgs.nodejs_20 # Specify Node.js version 20.x
+  ];
 }
